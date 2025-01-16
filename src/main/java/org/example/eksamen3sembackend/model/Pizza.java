bb0dab41d4e9eb58575fdcad8e8f2d3e6901071e
Aleksandergreg <alek3691@stud.kea.dk>
2025-01-16 09:30:07 +0100

    Created Pizza.java entity class

--- src/main/java/org/example/eksamen3sembackend/model/Pizza.java
+++ src/main/java/org/example/eksamen3sembackend/model/Pizza.java
@@ -1,24 +1,47 @@
 package org.example.eksamen3sembackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-
+import jakarta.persistence.*;
 
 @Entity
 public class Pizza {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pizzaId;
 
-    private Long id;
     private String title;
-    private double price;
+    private int price;
+
+    public Pizza() {
+    }
+
+    public Pizza(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+
+    public Long getPizzaId() {
+        return pizzaId;
+    }
+
+    public void setPizzaId(Long pizzaId) {
+        this.pizzaId = pizzaId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
