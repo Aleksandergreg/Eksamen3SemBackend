0cfafa8063822e69f312553284e5179552377314
Aleksandergreg <alek3691@stud.kea.dk>
2025-01-16 09:38:08 +0100

    Created StationRepository.java

---
+++ src/main/java/org/example/eksamen3sembackend/repository/StationRepository.java
@@ -0,0 +1,8 @@
+package org.example.eksamen3sembackend.repository;
+
+
+import org.example.eksamen3sembackend.model.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StationRepository extends JpaRepository<Station, Long> {
+}
