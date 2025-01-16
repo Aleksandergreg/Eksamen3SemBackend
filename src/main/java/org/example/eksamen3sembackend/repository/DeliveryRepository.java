debdc3c434ec05dc4043c0bd669706a029067835
Aleksandergreg <alek3691@stud.kea.dk>
2025-01-16 09:38:31 +0100

    Created DeliveryRepository.java

---
+++ src/main/java/org/example/eksamen3sembackend/repository/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package org.example.eksamen3sembackend.repository;
+
+import org.example.eksamen3sembackend.model.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
+
