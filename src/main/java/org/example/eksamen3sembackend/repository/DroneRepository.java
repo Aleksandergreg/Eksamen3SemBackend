1035479c0a2f54f9e82549ebd402e892da089b06
Aleksandergreg <alek3691@stud.kea.dk>
2025-01-16 09:38:24 +0100

    Created DroneRepository.java

---
+++ src/main/java/org/example/eksamen3sembackend/repository/DroneRepository.java
@@ -0,0 +1,8 @@
+package org.example.eksamen3sembackend.repository;
+
+import org.example.eksamen3sembackend.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DroneRepository extends JpaRepository<Drone, Long> {
+
+}
